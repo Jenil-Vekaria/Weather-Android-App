556a90f764c7810b07ac01dc94e6d43ef875a99a
Jenil-Vekaria <jvekaria@ryerson.ca>
2021-07-15 15:46:11 -0400

    Parcelize the Location data class with @Parcelize

--- app/src/main/java/com/example/weather_android_app/data/model/Location.kt
+++ app/src/main/java/com/example/weather_android_app/data/model/Location.kt
@@ -1,35 +1,11 @@
 package com.example.weather_android_app.data.model
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Location(
     val title: String,
     @Json(name="latt_long") val coordinate: String
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString()!!,
-        parcel.readString()!!
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(title)
-        parcel.writeString(coordinate)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Location> {
-        override fun createFromParcel(parcel: Parcel): Location {
-            return Location(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Location?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+): Parcelable
